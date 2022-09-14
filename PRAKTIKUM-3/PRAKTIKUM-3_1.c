2bc464226182149599a1f2b74fe7d12b6a56bc92
alimsatria <alimsatria2017@mail.ugm.ac.id>
2022-09-14 15:17:30 +0700

    small tweak

--- PRAKTIKUM-3/PRAKTIKUM-3_1.c
+++ PRAKTIKUM-3/PRAKTIKUM-3_1.c
@@ -1,8 +1,8 @@
 #include "GaussJordan.h"
 
-int input_function[3][3] = {0,    3,  7,
-                            -3,   0,  0,
-                            7,    0,  0};
+int input_function[3][3] = {    0,    3,  7,
+                                -3,   0,  0,
+                                7,    0,  0     };
 
 int output_x[3][3];
 int output_y[3][3];
@@ -10,26 +10,23 @@ int output_y[3][3];
 float nilai_x = 0.0;
 float nilai_y = 0.0;
 
-int turunan_x2[3][3];
-int turunan_y2[3][3];
-
 int main(void){
 
     printf("Input fungsi : \n");
     print_function_matrix(3,3,input_function);
 
-    // printf("turunan fungsi terhadap x : \n");
-    // get_differential_multivar(3,3,1,0,input_function,output_x);
-    // print_function_matrix(3,3,output_x);
-    // printf("turunan fungsi terhadap y : \n");
-    // get_differential_multivar(3,3,0,1,input_function,output_y);
-    // print_function_matrix(3,3,output_y);
+    printf("turunan fungsi terhadap x : \n");
+    get_differential_multivar(3,3,1,0,input_function,output_x);
+    print_function_matrix(3,3,output_x);
+    printf("turunan fungsi terhadap y : \n");
+    get_differential_multivar(3,3,0,1,input_function,output_y);
+    print_function_matrix(3,3,output_y);
 
-    // nilai_x = ((float)output_x[0][0] / (float)output_x[0][1]) * -1 ;
-    // nilai_y = ((float)output_y[0][0] / (float)output_y[1][0]) * -1 ;
+    nilai_x = ((float)output_x[0][0] / (float)output_x[0][1]) * -1 ;
+    nilai_y = ((float)output_y[0][0] / (float)output_y[1][0]) * -1 ;
 
-    // printf("Nilai critical point x: %0.2f\n",nilai_x);
-    // printf("Nilai critical point y: %0.2f\n",nilai_y);
+    printf("Nilai critical point x: %0.2f\n",nilai_x);
+    printf("Nilai critical point y: %0.2f\n",nilai_y);
 
     int out = uji_definitess(3,3,input_function);
 
